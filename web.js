18d81cd3fdc4ecf5263b2303eb85eecf86d91b46
Ubuntu <ubuntu@ip-10-164-46-149.ec2.internal>
2013-07-09 19:04:14 +0000

    attempt to fix commit updating web.js to read from index.html

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var fs = require('fs');
   var buffer = fs.readFileSync('index.html', 'utf8');
-  response.send(buffer.toString());
+      response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
